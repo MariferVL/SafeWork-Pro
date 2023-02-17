20b686c2064ccee9d057364df8adf3b4885ba9bb
LuisZentenxx <luis.zenteno02@inacapmail.cl>
2023-02-16 21:45:08 -0300

    update

--- src/sprintempresa/Cliente.java
+++ src/sprintempresa/Cliente.java
@@ -18,7 +18,8 @@ public class Cliente extends Usuario {
     }
 
     public Cliente(String nombre, String fechaNacimiento, String rut, String nombres, String apellidos,
-            String telefono, String afp, String sistemaSalud, String direccion, String comuna, String edad) {
+            String telefono, String afp, String sistemaSalud, String direccion, String comuna, String edad)
+            {
         super(nombre, fechaNacimiento, rut);
         this.nombres = nombres;
         this.apellidos = apellidos;
